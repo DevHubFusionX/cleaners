7dc3da06c3f257d0e084f8102e18f0f6bc638116
@DevCruze <jeeyalex7@gmail.com>
2026-01-29 08:05:21 +0100

    Change div to Link for logo navigation

--- src/components/layout/Header/Logo.jsx
+++ src/components/layout/Header/Logo.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoImg from '../../../assets/comtranz/logo.png';
 
 const Logo = () => {
     return (
         <div className="flex items-center gap-2 group">
-            <div className="w-24 h-18 flex items-center justify-center transform group-hover:scale-105 transition-transform duration-300">
+            <Link to="/" className="w-24 h-18 flex items-center justify-center transform group-hover:scale-105 transition-transform duration-300">
                 <img src={logoImg} alt="Comtranz Global Logo" className="w-full h-full object-contain" />
-            </div>
-          
+            </Link>
         </div>
     );
 };
 
-export default Logo;
+export default Logo;;
